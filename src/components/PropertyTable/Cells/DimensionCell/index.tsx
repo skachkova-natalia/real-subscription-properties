dca628ab568bcce58f853f169d0dce63d40a3ad1
NataliaBorisova <natalia.k.borisova@stm-labs.ru>
2024-09-08 13:26:25 +0300

    select key

--- src/components/PropertyTable/Cells/DimensionCell/index.tsx
+++ src/components/PropertyTable/Cells/DimensionCell/index.tsx
@@ -3,10 +3,8 @@ import {MathJax, MathJaxContext} from 'better-react-mathjax';
 import {MATHJAX_DIMENSIONS} from '@src/constants';
 import {PropertyItem} from '@src/types/table';
 import {getTableRow} from '@models/propertyTable';
-import {useState} from 'react';
 
 export function DimensionCell(value: string, row: PropertyItem) {
-  const [key, setKey] = useState(true);
 
   const dimensionOptions = row.available_dimensions?.map((dimension) => ({
     key: dimension,
@@ -18,12 +16,10 @@ export function DimensionCell(value: string, row: PropertyItem) {
     <MathJaxContext>
       {row.available_dimensions && row.available_dimensions?.length > 0 && (
         <Select
-          key={`${key}`}
           options={dimensionOptions}
           defaultValue={row.available_dimensions[0]}
           onChange={(newValue) => {
             getTableRow({property_dimension: newValue, property: row.propertyId});
-            setKey((v) => !v);
           }}
         />
       )}

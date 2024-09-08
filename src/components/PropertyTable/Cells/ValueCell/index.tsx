670693eaa6328f0bb714175a3c9acb8eba8d863b
NataliaBorisova <natalia.k.borisova@stm-labs.ru>
2024-09-08 13:52:26 +0300

    number toExponential

--- src/components/PropertyTable/Cells/ValueCell/index.tsx
+++ src/components/PropertyTable/Cells/ValueCell/index.tsx
@@ -1,16 +1,18 @@
 import {Tooltip} from 'antd';
 
 export function ValueCell(value: string) {
-  const getValue = ()=>{
-    if (value==='NaN') {
+  const getValue = () => {
+    if (value === 'NaN') {
       return '—';
     }
-    if (value==='Infinity') {
+    if (value === 'Infinity') {
       return '∞';
     }
     return value;
-  }
+  };
+  console.log(value, Number.parseFloat(value).toExponential(3));
+
   return (
-   <Tooltip title={getValue()}>{Number(getValue()).toFixed(3)}</Tooltip>
+    <Tooltip title={getValue()}>{Number.parseFloat(value).toExponential(3)}</Tooltip>
   );
 }

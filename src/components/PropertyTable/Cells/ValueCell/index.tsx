fdd6a548a17ac6d33af14218951cccfd9ec40784
NataliaBorisova <natalia.k.borisova@stm-labs.ru>
2024-12-23 09:56:14 +0300

    copying value to clipboard [RSP-5]

--- src/components/PropertyTable/Cells/ValueCell/index.tsx
+++ src/components/PropertyTable/Cells/ValueCell/index.tsx
@@ -1,4 +1,5 @@
 import {Tooltip} from 'antd';
+import * as S from './styled';
 
 export function ValueCell(value: string) {
   const getValue = () => {
@@ -12,6 +13,10 @@ export function ValueCell(value: string) {
   };
 
   return (
-    <Tooltip title={getValue()}>{Number.parseFloat(value).toExponential(3)}</Tooltip>
+    <Tooltip title={getValue()}>
+      <S.Value onClick={() => navigator.clipboard.writeText(value)}>
+        {Number.parseFloat(value).toExponential(3)}
+      </S.Value>
+    </Tooltip>
   );
 }

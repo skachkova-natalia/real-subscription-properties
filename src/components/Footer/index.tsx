f682508c836997146face05bbffd3ff446a79ad3
NataliaBorisova <natalia.k.borisova@stm-labs.ru>
2025-02-11 14:41:52 +0300

    change contacts [RSP-79]

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import TelegramIconSvg from '@assets/telegramIcon.svg?react';
 import {color} from '@src/theme';
+import {Typography} from 'antd';
 import * as S from './styled';
 
 export function Footer() {
@@ -8,9 +9,12 @@ export function Footer() {
       <S.A href='https://t.me/konovalov_ia'>
         <TelegramIconSvg width={20} height={20} fill={color.base.s600} />
       </S.A>
-      <S.A href='mailto:iliakonowaloff@yandex.ru'>
-        iliakonowaloff@yandex.ru
-      </S.A>
+      <Typography.Text>
+        Остались вопросы?{' '}
+        <S.A href='mailto:support@rsp.ru'>
+          Напишите
+        </S.A> нам
+      </Typography.Text>
     </S.Container>
   );
 }

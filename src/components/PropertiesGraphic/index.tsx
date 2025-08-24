6a1c52e10709dda15790c0df0e2e0a9682c277e6
NataliaBorisova <natalia.k.borisova@stm-labs.ru>
2025-08-25 00:33:56 +0300

    brush values

--- src/components/PropertiesGraphic/index.tsx
+++ src/components/PropertiesGraphic/index.tsx
@@ -1,5 +1,15 @@
 import * as S from './styled';
-import {Legend, Line, LineChart, ResponsiveContainer, Tooltip, TooltipContentProps, XAxis, YAxis} from 'recharts';
+import {
+  Brush,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  TooltipContentProps,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import GraphicFilters from '@components/PropertiesGraphic/GraphicFilters';
 import {$graphic} from '@models/propertiesGraphic';
 import {useUnit} from 'effector-react';
@@ -9,7 +19,15 @@ import i18n from 'i18next';
 import {NameType, ValueType} from 'recharts/types/component/DefaultTooltipContent';
 
 export default function PropertiesGraphic() {
-  const {points, selectedProperty, variableParameter, fixedParameter, fixedParameterValues, loading, error} = useUnit($graphic);
+  const {
+    points,
+    selectedProperty,
+    variableParameter,
+    fixedParameter,
+    fixedParameterValues,
+    loading,
+    error,
+  } = useUnit($graphic);
 
   const lineKeys = points.length > 0
     ? Object.keys(points[0]).filter(key => key !== 'x')
@@ -43,15 +61,20 @@ export default function PropertiesGraphic() {
       {error && (<S.Error>{error.msg[`${i18n.language}`]}</S.Error>)}
       {points.length > 0 && (
         <Spin indicator={<LoadingOutlined spin />} spinning={loading}>
-          <ResponsiveContainer width='100%' minHeight={300}>
-            <LineChart data={points}>
+          <ResponsiveContainer width='100%' minHeight={350}>
+            <LineChart data={points} margin={{  right: 10, bottom: 10}}>
+              <Legend
+                verticalAlign='top'
+                height={36}
+                formatter={(value) => <span
+                  style={{color: '#333'}}>{fixedParameter} = {fixedParameterValues[value]}</span>}
+              />
               <XAxis
                 dataKey='x'
                 tickFormatter={formatXAxis}
                 label={{value: `${variableParameter}`, position: 'insideBottomRight', offset: -5}}
               />
               <YAxis label={{value: `${selectedProperty}`, position: 'insideTopLeft'}} />
-              <Tooltip content={customTooltip} />
               {lineKeys.map((key, index) => (
                 <Line
                   key={key}
@@ -62,11 +85,27 @@ export default function PropertiesGraphic() {
                   activeDot={{r: 4}}
                 />
               ))}
-              <Legend
-                verticalAlign="top"
-                height={36}
-                formatter={(value) => <span style={{ color: '#333' }}>{fixedParameter} = {fixedParameterValues[value]}</span>}
-              />
+              <Tooltip content={customTooltip} />
+                <Brush
+                  dataKey='x'
+                  height={1}
+                  stroke='#0376BE'
+                  fill='transparent'
+                  traveller={(props) => {
+                    const {x, y} = props;
+                    return (
+                      <rect
+                        x={x}
+                        y={y - 6}
+                        width={12}
+                        height={12}
+                        fill='#0376BE'
+                        rx={15}
+                        ry={15}
+                      />
+                    );
+                  }}
+                />
             </LineChart>
           </ResponsiveContainer>
         </Spin>

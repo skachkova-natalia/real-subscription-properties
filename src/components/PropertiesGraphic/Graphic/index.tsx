ebad3a03d7d2201a839bb4ed232e133994532dda
NataliaBorisova <natalia.k.borisova@stm-labs.ru>
2025-10-12 20:34:54 +0300

    react-chartjs-2 [RSP-157]

--- src/components/PropertiesGraphic/Graphic/index.tsx
+++ src/components/PropertiesGraphic/Graphic/index.tsx
@@ -1,106 +1,140 @@
-import {
-  Brush,
-  Legend,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  TooltipContentProps,
-  XAxis,
-  YAxis,
-} from 'recharts';
-import {NameType, ValueType} from 'recharts/types/component/DefaultTooltipContent';
-import * as S from './styled';
-import {useUnit} from 'effector-react';
-import {$graphic} from '@models/propertiesGraphic';
 import {Point} from '@src/types/graphic';
+import React, {useMemo} from 'react';
+import {Line} from 'react-chartjs-2';
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  ChartData,
+  ChartOptions,
+  Filler,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import {useUnit} from 'effector-react';
+import {$filters} from '@models/filters';
+import i18n from 'i18next';
+import {$graphic} from '@models/propertiesGraphic';
+import * as S from './styled';
+import ZoomPlugin from 'chartjs-plugin-zoom';
 
 interface Props {
   property: string;
   points: Point[];
 }
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  ZoomPlugin,
+);
+
 export default function Graphic({property, points}: Props) {
+  const {modesParams, propertiesList} = useUnit($filters);
   const {
-    variableParameter,
     fixedParameter,
     fixedParameterValues,
+    variableParameter,
   } = useUnit($graphic);
-
-  const lineKeys = points.length > 0
-    ? Object.keys(points[0]).filter(key => key !== 'x')
-    : [];
-
-  const lineColors = ['#0376BE', '#FF7F0E', '#2CA02C', '#D62728', '#9467BD', '#8C564B', '#7F7F7F', '#BCBD22', '#17BECF'];
-
-  const customTooltip = ({active, payload, label}: TooltipContentProps<ValueType, NameType>) => {
-    if (active && payload?.length) {
-      return (
-        <S.CustomTooltip>
-          {`${variableParameter}: ${label}`}<br />
-          {payload.map((entry, index) => (
-            <div key={index} style={{color: entry.color}}>
-              {`${property} (при ${fixedParameter}=${fixedParameterValues[property]}): ${Number(entry.value).toFixed(5)}`}
-            </div>
-          ))}
-        </S.CustomTooltip>
-      );
+  const propertyName: string = useMemo(() => {
+    const prop = propertiesList.find((prop) => prop.literal === property);
+    if (!prop) {
+      return property;
     }
-    return null;
+    return `${prop.name[`${i18n.language}`]} (${prop.literal})`;
+  }, []);
+
+  const variableParameterName: string = useMemo(() => {
+    const prop = modesParams.find((prop) => prop.id === variableParameter);
+    if (!prop) {
+      return variableParameter;
+    }
+    return `${prop.name[`${i18n.language}`]} (${prop.id})`;
+  }, [fixedParameter]);
+
+  const chartData: ChartData<'line'> = {
+    datasets: [
+      {
+        label: `${fixedParameter} = ${fixedParameterValues[property]}`,
+        data: points,
+        borderColor: 'rgb(59, 130, 246)',
+        backgroundColor: 'rgba(59, 130, 246, 0.1)',
+        borderWidth: 2,
+        fill: true,
+        tension: 0.4,
+        pointRadius: 0,
+        pointHoverRadius: 5,
+        borderJoinStyle: 'round',
+      },
+    ],
   };
 
-  const formatXAxis = (tickItem) => {
-    return Number(tickItem).toFixed(2);
+  const options: ChartOptions<'line'> = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+      zoom: {
+        pan: {
+          enabled: true,
+          mode: 'x',
+        },
+        zoom: {
+          wheel: {
+            enabled: true,
+          },
+          pinch: {
+            enabled: true,
+          },
+          mode: 'x',
+        },
+        limits: {
+          x: {min: 'original', max: 'original'},
+          y: {min: 'original', max: 'original'},
+        },
+      },
+    },
+    scales: {
+      x: {
+        type: 'linear',
+        title: {
+          display: true,
+          text: variableParameterName,
+        },
+      },
+      y: {
+        type: 'linear',
+        title: {
+          display: true,
+          text: propertyName,
+        },
+      },
+    },
+    interaction: {
+      mode: 'nearest',
+      axis: 'x',
+      intersect: false,
+    },
+    animation: {
+      duration: 0,
+    },
   };
 
   return (
-    <ResponsiveContainer width='100%' minHeight={350}>
-      <LineChart data={points} margin={{  right: 10, bottom: 10}}>
-        <Legend
-          verticalAlign='top'
-          height={36}
-          formatter={() => (
-            <span style={{color: '#333'}}>{fixedParameter} = {fixedParameterValues[property]}</span>
-          )}
-        />
-        <XAxis
-          dataKey='x'
-          tickFormatter={formatXAxis}
-          label={{value: `${variableParameter}`, position: 'insideBottomRight', offset: -5}}
-        />
-        <YAxis label={{value: `${property}`, position: 'insideTopLeft'}} />
-        {lineKeys.map((key, index) => (
-          <Line
-            key={key}
-            type='monotone'
-            dataKey={key}
-            stroke={lineColors[index % lineColors.length]}
-            dot={false}
-            activeDot={{r: 4}}
-          />
-        ))}
-        <Tooltip content={customTooltip} />
-        <Brush
-          dataKey='x'
-          height={1}
-          stroke='#0376BE'
-          fill='transparent'
-          traveller={(props) => {
-            const {x, y} = props;
-            return (
-              <rect
-                x={x}
-                y={y - 6}
-                width={12}
-                height={12}
-                fill='#0376BE'
-                rx={15}
-                ry={15}
-              />
-            );
-          }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
-  )
+    <S.GraphicContainer>
+      <Line data={chartData} options={options} />
+    </S.GraphicContainer>
+  );
 }
--- src/components/PropertiesGraphic/Graphic/styled.ts
+++ src/components/PropertiesGraphic/Graphic/styled.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
-import {color} from '@src/theme';
 
-export const CustomTooltip = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  background-color: ${color.base.s0};
+export const GraphicContainer = styled.div`
+  height: 300px;
 `;
